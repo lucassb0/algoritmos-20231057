dbb0ab3cb63bea838dcb2636154784b0cb15187b
Lucas Bezerra <a95090242@rn.senac.local>
2023-06-26 21:26:57 -0300

    primeiros passos exemplo switch

---
+++ javascript/exemplo-switch.js
@@ -0,0 +1,10 @@
+let letra = 'i'
+
+switch (letra) {
+    case 'a': case 'e': case 'i': case 'o': case 'u': 
+        alert('É uma vogal');
+        break;
+    default:
+        alert('Não é vogal')
+    
+}
